f53439dbf539677b3a8fd3d85217a93bd0670935
Javad Asgari Shafique <javadshafique@hotmail.com>
2021-03-12 18:49:44 +0100

    added screen clear + example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use tui::Terminal;
+use tui::backend::CrosstermBackend;
+use tui::widgets::{Widget, Clear, Block, Borders};
+use tui::layout::{Layout, Constraint, Direction};
+use std::process::Command;
+
+
+fn main() -> Result<(), io::Error> {
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    print!("{}", "\x1B[2J\x1B[1;1H");
+    terminal.draw(|f| {   
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(1)
+            .constraints(
+                [
+                    Constraint::Percentage(10),
+                    Constraint::Percentage(80),
+                    Constraint::Percentage(10)
+                ].as_ref()
+            )
+            .split(f.size());
+        
+        
+            
+        let block = Block::default()
+             .title("Block")
+             .borders(Borders::ALL);
+        f.render_widget(block, chunks[0]);
+        let block = Block::default()
+             .title("Block 2")
+             .borders(Borders::ALL);
+        f.render_widget(block, chunks[1]);
+    })
 }
