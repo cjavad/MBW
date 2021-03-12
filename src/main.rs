40a84e417e2cd775feb43447a537e7a72b1b6cb7
= <hjalte.nannestad@gmail.com>
2021-03-12 19:36:26 +0100

    Switched to bracket-lib

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,25 @@
-use std::io;
-use tui::Terminal;
-use tui::backend::CrosstermBackend;
-use tui::widgets::{Widget, Clear, Block, Borders};
-use tui::layout::{Layout, Constraint, Direction};
-use std::process::Command;
+use bracket_lib::prelude::*;
 
+pub struct State {
 
-fn main() -> Result<(), io::Error> {
-    let stdout = io::stdout();
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-    print!("{}", "\x1B[2J\x1B[1;1H");
-    terminal.draw(|f| {   
-        let chunks = Layout::default()
-            .direction(Direction::Vertical)
-            .margin(1)
-            .constraints(
-                [
-                    Constraint::Percentage(10),
-                    Constraint::Percentage(80),
-                    Constraint::Percentage(10)
-                ].as_ref()
-            )
-            .split(f.size());
-        
-        
-            
-        let block = Block::default()
-             .title("Block")
-             .borders(Borders::ALL);
-        f.render_widget(block, chunks[0]);
-        let block = Block::default()
-             .title("Block 2")
-             .borders(Borders::ALL);
-        f.render_widget(block, chunks[1]);
-    })
+}
+
+impl State {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error + 'static + Send + Sync>> {
+    let ctx = BTermBuilder::simple(200, 100)?.with_title("MBW").build()?;
+
+    let state = State::new();
+
+    main_loop(ctx, state)
 }
