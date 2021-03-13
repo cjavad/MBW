14c3ad4bacd00dc8a08a003f90467ee1526f1297
Javad Asgari Shafique <javadshafique@hotmail.com>
2021-03-13 15:19:16 +0100

    Added ClientNetworkHandle to game client

--- src/state.rs
+++ src/state.rs
@@ -1,14 +1,16 @@
 use crate::map;
 use crate::map_generation;
 use crate::structures;
+use crate::client::ClientNetworkHandle;
 use bracket_lib::prelude::*;
 
 pub struct State {
     pub map: map::Map,
+    pub handle: ClientNetworkHandle,
 }
 
 impl State {
-    pub fn new() -> Self {
+    pub fn new(handle: ClientNetworkHandle) -> Self {
         let settings = map_generation::MapGenerationSettings {
             width: 24,
             height: 16,
@@ -19,6 +21,7 @@ impl State {
 
         Self {
             map: settings.generate(&mut rng),
+            handle,
         }
     }
 }
